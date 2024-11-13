fa70052d4589e89a60bd207e005de0b5b9b0b919
Bruno Dias <bruno.dias@akiyama.com.br>
2024-11-13 14:17:01 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿using System.IO;
+using System.Configuration;
+using System.Data;
+using WatsonWebserver;
+using WatsonWebserver.Core;
+using System.Windows;
+
+namespace WindowsServer
+{
+    public class Program
+    {
+	// All WPF applications should execute on a single-threaded apartment (STA) thread
+	[STAThread]
+	public static void Main()
+	{
+	    App app = new App();
+	    app.Run();
+	}
+    }
+
+    public class App : Application
+    {
+
+	static async Task DefaultRoute(HttpContextBase ctx)
+	{
+	    try
+	    {
+
+		Application.Current.Dispatcher.Invoke((Action)delegate{
+			Window window = new Window();
+			window.Show();
+		    });
+
+		await ctx.Response.Send("ok");
+	    }
+	    catch (Exception ex)
+	    {
+		Console.WriteLine(ex.Message);
+	    }
+	}
+
+	protected override void OnStartup(StartupEventArgs e)
+	{
+	    base.OnStartup(e);
+
+
+	    WebserverSettings _Settings = new WebserverSettings
+	    {
+		Hostname = "localhost",
+		Port = 8000
+	    };
+
+	    Webserver server = new Webserver(_Settings, DefaultRoute);
+
+	    server.Start();
+	}
+    }
+}
